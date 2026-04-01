31480488426f925db2b59e8dd2a5722d098e445e
Tortoha <iskandersuleiemenov@gmail.com>
2026-04-01 16:29:38 +0500

    I downloaded and installed the npm package for integration with the CRM

--- Magaz/app/src/api.js
+++ Magaz/app/src/api.js
@@ -1,4 +1,5 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/torta-js/+esm';
+// import { createClient } from 'https://cdn.jsdelivr.net/npm/torta-js/+esm';
+import { createClient } from "torta-js";
 
 const API_KEY = "a3f8d2e1b4c7f9a0e5d3b6c8f2a1e4d7b9c0f3a6e8d1b5c7f0a2e9d4b8c6f1a3";
 
